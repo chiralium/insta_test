2fe49250a080a50970c25236a2f1cb465d9fe873
elexpr <be2e@yandex.ru>
2021-01-11 00:02:38 +0300

    SQL

--- sql/insta_insta_user_contacts.sql
+++ sql/insta_insta_user_contacts.sql
@@ -1,4 +1,4 @@
 INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (1, 1, 2);
-INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (2, 1, 1);
-INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (3, 1, 4);
-INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (4, 1, 5);
+INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (6, 1, 5);
+INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (7, 10, 3);
+INSERT INTO insta.insta_user_contacts (id, user_id, contact_id) VALUES (8, 3, 2);
